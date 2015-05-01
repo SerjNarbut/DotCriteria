e1e38cccc9ceb6c47f970de0638f2c4586809e34
Serj Narbut <serjnarbut@gmail.com>
2015-05-02 00:55:48 +0300

    Parameter Swapper for Expression trees

---
+++ DotCriteria/Criterias/Criteria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotCriteria.Criterias
+{
+    public class Criteria<T> : ICriteria<T>
+    {
+        public System.Linq.Expressions.Expression<Func<T, bool>> Lambda { get; private set; }
+
+        public ICriteria<T> Or(ICriteria<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICriteria<T> And(ICriteria<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICriteria<T> Or(Expression<Func<T, bool>> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICriteria<T> And(Expression<Func<T, bool>> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Func<T, bool> Compile()
+        {
+            return Lambda.Compile();
+        }
+    }
+}
---
+++ DotCriteria/Utils/SwapParams.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotCriteria.Utils
+{
+    internal class SwapParams : ExpressionVisitor
+    {
+        private readonly Expression from;
+        private readonly Expression to;
+
+        public SwapParams(Expression from, Expression to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public override Expression Visit(Expression node)
+        {
+            if (node == from)
+                return to;
+            return base.Visit(node);
+        }
+
+        public static Expression ReplaceParam(Expression body, ParameterExpression from, ParameterExpression to)
+        {
+            return new SwapParams(from, to).Visit(body);
+        }
+    }
+}
