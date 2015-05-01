f02834ac7480b4079576707d48215d5ce5af1ce5
Serj Narbut <serjnarbut@gmail.com>
2015-05-02 01:20:54 +0300

    CacheCompiledCriteria class

---
+++ DotCriteria/Criterias/CacheCompiledCriteria.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotCriteria.Criterias
+{
+    public class CacheCriteria<T> : Criteria<T>
+    {
+        private Func<T, bool> predicate;
+        public bool IsCompiled { get; private set; }
+
+        public CacheCriteria(Expression<Func<T, bool>> lambda)
+            : base(lambda)
+        {
+            predicate = null;
+            IsCompiled = false;
+        }
+
+        public override Func<T, bool> Compile()
+        {
+            if(!IsCompiled)
+            {
+                predicate = base.Compile();
+                IsCompiled = true;
+            }
+            return predicate;
+        }
+    }
+}
