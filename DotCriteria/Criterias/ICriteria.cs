47d1a643c8d0f5425d9cc5a44924545849cae66c
Serj Narbut <serjnarbut@gmail.com>
2015-05-02 00:47:22 +0300

    ICriteria interface

---
+++ DotCriteria/Criterias/ICriteria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotCriteria.Criterias
+{
+    public interface ICriteria<T>
+    {
+        Expression<Func<T, bool>> Lambda { get; }
+
+        ICriteria<T> Or(ICriteria<T> other);
+        ICriteria<T> And(ICriteria<T> other);
+
+        ICriteria<T> Or(Expression<Func<T, bool>> other);
+        ICriteria<T> And(Expression<Func<T, bool>> other);
+
+        Func<T, bool> Compile();
+    }
+}
